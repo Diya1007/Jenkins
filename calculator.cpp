2f8ee22c3e96b84340dea84ac3618ad02eca649e
Diya1007 <diyaothers@gmail.com>
2026-09-14 15:39:06 -0400

    creating the pipeline

---
+++ calculator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int add(int a,int b)
+{
+    return a+b;
+}
+int main(){
+    if(add(2,3)==5)
+    {
+        cout<<"Test passed";
+        return 0;
+    }
+    else
+    return 1;
+
+}
